5916ede6c6d7b2bdd7d9e0297099325d652647cc
Artjom_Stalev <stalev.artjom@gmail.com>
2023-02-18 11:19:34 +0100

    migration was moved to core service

--- hardcore-service/src/main/resources/db/migration/U1__init.sql
+++
@@ -1,7 +0,0 @@
-drop table users_roles;
-drop table roles;
-drop table orders;
-drop table carts;
-drop table products;
-drop table categories;
-drop table users;
--- hardcore-service/src/main/resources/db/migration/V1__init.sql
+++ hardcore-service/src/main/resources/db/migration/V1__init.sql
@@ -35,3 +35,28 @@ create table order_items
     quantity                int,
     total_price             decimal(12, 2)
 );
+
+
+create table users (
+    id bigserial primary key,
+    username varchar(36) not null unique,
+    password varchar(255) not null );
+
+create table roles (
+    id bigserial primary key,
+    "name" varchar(50) not null);
+
+create table users_roles (
+    user_id bigint not null references users (id),
+    role_id bigint not null references roles (id),
+    primary key (user_id, role_id));
+
+
+insert into roles (name) values ('ROLE_USER'), ('ROLE_ADMIN');
+
+insert into users (username, password) values
+    ('artjom', '1234');
+
+insert into users_roles (user_id, role_id)
+    values (1, 1);
+
